1f2de35ec9a230b5cfba58ac1987f18b38a8a83e
Vedant Kawade <vedantkawade.official@gmail.com>
2025-01-11 20:46:08 +0530

    Update face-detection.py

--- face-detection.py
+++ face-detection.py
@@ -1,5 +1,5 @@
 import cv2
-face_cap = cv2.CascadeClassifier("haarcascade_frontalface.xml")
+face_cap = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
